9849d9d63c6c09b871b43354ba7d12d0f847f8be
vedavyas <vedavyas14042003@gmail.com>
2023-10-23 17:23:28 +0530

    Added the navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import LoginPage from "@/components/LoginPage";
-import OtpPage from "@/components/OtpPage";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <LoginPage />
     </main>
   );
